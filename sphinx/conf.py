b7616bca841e33771a1c7886d9cda8432c315de1
Alex Sonea <Alex@Alex3.local>
2019-11-20 23:48:24 +0000

    reorganised the docs folder

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Alex Sonea'
 author = 'alex.sonea@gmail.com'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
