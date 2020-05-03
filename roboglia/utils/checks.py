052f7f4af69e1d3bd5ddd0e2bb22c1f629e01044
sonelu <alex.sonea@gmail.com>
2020-05-03 11:29:00 +0100

    added licence to project and files

--- roboglia/utils/checks.py
+++ roboglia/utils/checks.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2020  Alex Sonea
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 def check_key(key, dict_info, context, context_id, logger, message=None):
     """Checks if a `key` is in a dictionary `dict_info` and raises a cusomized
     exception message with better context.
